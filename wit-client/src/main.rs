b705e98900751c0a475de1148bf3b64b2ceaf45f
Kartik Kapoor <kartikkapoor3390@gmail.com>
2024-02-12 17:31:22 -0600

    current status : composed wasm working

--- wit-client/src/main.rs
+++ wit-client/src/main.rs
@@ -12,32 +12,27 @@ use wasmtime::{self, component::{Component, Linker}, Config, Engine, Store};
 use wasmtime::component::__internal::wasmtime_environ::object::U32;
 use wasmtime::component::{ComponentNamedList, Val};
 
-mod req;
-mod resp;
-
 wasmtime::component::bindgen!(
     {
-        world:"host",
-        path: "../wit-server/wit/host.wit",
+        world:"example",
+        path: "world.wit",
     }
 );
 
-wasmtime::component::bindgen!(
-    {
-        world:"second",
-        path: "../wit-server/wit/host.wit",
-    }
-);
+
+// wasmtime::component::bindgen!(
+//     {
+//         world:"host",
+//         path: "../wit-server/wit/host.wit",
+//     }
+// );
 
 
 
 static DEFAULT_PORT: &str = "8080";
-pub struct WASM{
-    pub s : Store<Fib>,
-    pub host : Host_
-}
+
 pub struct Fib;
-// impl Host_Imports for Fib {
+// impl HostImports for Fib {
 //     fn run(&mut self, n:u32)->wasmtime::Result<u32>{
 //         fn x(j:u32)->u32{
 //             if j <=0{
@@ -72,26 +67,36 @@ async fn reply() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
 
 
         let mut linker = Linker::new(&engine);
-        // Host_::add_to_linker(&mut linker, |state: &mut Fib| state);
-
 
         let component = Component::from_file(&engine, "../wit-server/my-component.wasm").unwrap();
-        let mut host_instance = linker.root();
-        let fib = Component::from_file(&engine, "../wit-server2/fib.wasm").unwrap();
+        let composed = Component::from_file(&engine, "../composed/composed.wasm").unwrap();
 
-        host_instance.func_new(&component, "run", |_,params, results|{
-            let Val::U32(mut value) = params[0] else { panic!("not possible") };
-            value*= 2;
-            results[0] = Val::U32(value);
-            Ok(())
-        }).expect("TODO: panic message");
-        let host = linker.instantiate(&mut store, &component).expect("pacnhod");
+        let cargocomp = Component::from_file(&engine, "../proxy/target/wasm32-unknown-unknown/release/proxy.wasm").unwrap();
+        let (example,_) = Example::instantiate(&mut store, &composed, &linker).expect("why not");
+    //     let mut host_instance = linker.root();
+    //     let fib = Component::from_file(&engine, "../wit-server2/fib.wasm").unwrap();
+    //
+    //     host_instance.func_new(&component, "run", |_,params, results|{
+    //         let Val::U32(mut value) = params[0] else { panic!("not possible") };
+    //         value*= 2;
+    //         results[0] = Val::U32(value);
+    //         Ok(())
+    //     }).expect("TODO: panic message");
+    //     let host = linker.instantiate(&mut store, &component).expect("pacnhod");
+    //
+    //     let fib_instance = linker.instantiate(&mut store, &fib).expect("pacnhod");
+        let preinstantiate = linker.instantiate_pre(&cargocomp).expect("TODO: panic message");
 
-        let fib_instance = linker.instantiate(&mut store, &fib).expect("pacnhod");
-        let run = host.get_typed_func::<(u32,),(u32,)>(&mut store, "execute").expect("wasnt a func");
-        let fib_func = fib_instance.get_typed_func::<(u32,),(u32,)>(&mut store, "fib").expect("something wrong");
-        let k = run.call(&mut store,(10,)).expect("aoye");
-        let y = fib_func.call(&mut store, (1,)).expect("p");
+        let hello_instance = preinstantiate.instantiate(&mut store).expect("pacnhod");
+        let r = example.interface0.call_run(&mut store, 2).expect("not hey");
+        // let exp = hello_instance.exports(&mut store);
+    //
+    //     let run = host.get_typed_func::<(u32,),(u32,)>(&mut store, "execute").expect("wasnt a func");
+    //     let fib_func = fib_instance.get_typed_func::<(u32,),(u32,)>(&mut store, "fib").expect("something wrong");
+    // let hello_func = hello_instance.proxy().get_typed_func::<(u32,),(u32,)>(&mut store, "middleware").expect("something wrong");
+    //     let he = hello_func.call(&mut store,(10,)).expect("not working helo");
+    // // let k = run.call(&mut store,(10,)).expect("aoye");
+    //     let y = fib_func.call(&mut store, (1,)).expect("p");
         // let (host,_) = Host_::instantiate(&mut store, &component, &linker).unwrap();
 
 
@@ -107,7 +112,7 @@ async fn reply() -> Result<Box<dyn warp::Reply>, warp::Rejection> {
         // let z = second.call_count(&mut store).expect("abcd");
 
 
-        Ok(Box::new(format!("sup {} {}",k.0,y.0)))
+        Ok(Box::new(format!("sup {}",r)))
 }
 #[tokio::main]
 async fn main() {
@@ -116,6 +121,6 @@ async fn main() {
 
     let routes = warp::get().and(warp::path("wasm")).and_then(reply);
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3031)).await;
+    warp::serve(routes).run(([127, 0, 0, 1], 3032)).await;
 
 }
